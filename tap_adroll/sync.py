68a20af7b3fafcbf7745840254e0d229f8f75ee8
Adam <przywarty.it@gmail.com>
2020-05-26 08:09:31 +0200

    Move pagination stop condition to date chunker

--- tap_adroll/sync.py
+++ tap_adroll/sync.py
@@ -25,6 +25,9 @@ def date_chunks(
 
     while True:
         end_date = min(start + increment, maximum)
+        if start == end_date:
+            # API raises 400 if the start and end dates are identical.
+            return
         yield (start, end_date)
         start = end_date
         if end_date == maximum:
@@ -174,9 +177,6 @@ class AdRoll:
             increment=timedelta(weeks=26),
             maximum=campaign_end_date,
         ):
-            if start_date == end_date:
-                # API raises 400 if the start and end dates are identical.
-                break
             eid = campaign["eid"]
             LOGGER.info(
                 f"(advancing) campaign: {eid} start_date: {start_date} end_date: {end_date}"
